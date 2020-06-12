4c1341d3e7fef2a9f3966678e1e20a824aed2d68
trananhkma <trananhkma@gmail.com>
2020-06-12 23:40:58 +0700

    make color

--- src/containers/App/index.js
+++ src/containers/App/index.js
@@ -9,7 +9,6 @@ function App() {
     <div className="App">
       <header className="App-header">
         <h1>React Hacking</h1>
-        <img src={logo} className="App-logo" alt="logo" />
         <div class="pull-left">
           <NavLink className="App-link row" to="/hack1">Homework 1: PropsState</NavLink>
           <NavLink className="App-link row" to="/hack2">Homework 2: Call API with Axios</NavLink>

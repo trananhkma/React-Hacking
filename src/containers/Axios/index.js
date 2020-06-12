03b94b22f9a1e8c08bb286e62d0f2bbbc890f929
trananhkma <trananhkma@gmail.com>
2020-06-12 22:26:29 +0700

    darkmode: on

--- src/containers/Axios/index.js
+++ src/containers/Axios/index.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import PostList from "../../components/PostList"
+import styles from './style.module.css'
+
 
 const Axios = () => {
   return (
-    <div className="App">
-      <h3>JSON Placeholder</h3>
+    <div className={`App ${styles.bgColor}`}>
+      <h3 className={styles.title}>JSON Placeholder</h3>
       <PostList/>
     </div>
   );

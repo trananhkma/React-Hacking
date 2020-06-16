b80ca3e5c13194dd640621a7de689c1d893e190a
trananhkma <trananhkma@gmail.com>
2020-06-16 22:53:40 +0700

    clean

--- src/containers/NotFoundPage/index.js
+++ src/containers/NotFoundPage/index.js
@@ -1,23 +1,23 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import styles from './style.module.css'
 import Container from 'react-bootstrap/Container'
 
 
 const NotFoundPage = () => {
-    return (
-        <Container>
-        <div id="notfound">
-            <div className={styles.notfound}>
-                <div className={styles.notfound_404}>
-                    <h1>Oops!</h1>
-                </div>
-                <h2>404 - Page not found</h2>
-                <p>The page you are looking for might have been removed had its name changed or is temporarily unavailable.</p>
-                <a href="/">Go To Homepage</a>
-            </div>
+  return (
+    <Container>
+      <div id="notfound">
+        <div className={styles.notfound}>
+          <div className={styles.notfound_404}>
+            <h1>Oops!</h1>
+          </div>
+          <h2>404 - Page not found</h2>
+          <p>The page you are looking for might have been removed had its name changed or is temporarily unavailable.</p>
+          <a href="/">Go To Homepage</a>
         </div>
-        </Container>
-    )
+      </div>
+    </Container>
+  )
 }
 
 export default NotFoundPage;
